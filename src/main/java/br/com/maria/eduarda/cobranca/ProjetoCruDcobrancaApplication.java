6ecd6a0bc55beaec904d486969b470f24750acc2
mariasantosdev <mariaeduardasrodolfo2@gmail.com>
2021-03-15 15:17:59 -0300

    Front página de Cadastro de Títulos utilizando bootstrap

---
+++ src/main/java/br/com/maria/eduarda/cobranca/ProjetoCruDcobrancaApplication.java
@@ -0,0 +1,13 @@
+package br.com.maria.eduarda.cobranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCruDcobrancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCruDcobrancaApplication.class, args);
+	}
+
+}
