2f07f8b660aab36a098d3ef8c9d40c2e4a18e051
mariasantosdev <mariaeduardasrodolfo2@gmail.com>
2021-03-27 16:55:01 -0300

    Listando os títulos

--- src/main/java/br/com/maria/eduarda/cobranca/controller/TituloController.java
+++ src/main/java/br/com/maria/eduarda/cobranca/controller/TituloController.java
@@ -3,7 +3,6 @@ package br.com.maria.eduarda.cobranca.controller;
 import java.util.List;
 import java.util.Arrays;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,17 +17,16 @@ import br.com.maria.eduarda.cobranca.repository.Titulos;
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
-	
+
 	@Autowired
 	private Titulos titulos;
-	
+
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		return mv;
-		
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView salvar(Titulo titulo) {
 		titulos.save(titulo);
@@ -37,16 +35,17 @@ public class TituloController {
 		System.out.println(titulo);
 		return mv;
 	}
-	
+
 	@RequestMapping
-	public String pesquisar(){
-		return "PesquisaTitulos";
+	public ModelAndView pesquisar() {
+		List <Titulo> todosTitilos =  titulos.findAll();
+		ModelAndView mv = new ModelAndView("PesquisaTitulos");
+		mv.addObject("titulos", todosTitilos);
+		return mv;
 	}
-	
+
 	@ModelAttribute("todosStatusTitulo")
-	public List<StatusTitulo> todosStatusTitulo(){
+	public List<StatusTitulo> todosStatusTitulo() {
 		return Arrays.asList(StatusTitulo.values());
 	}
-	
-	
 }
--- src/main/java/br/com/maria/eduarda/cobranca/model/Titulo.java
+++ src/main/java/br/com/maria/eduarda/cobranca/model/Titulo.java
@@ -59,7 +59,10 @@ public class Titulo {
 	public void setStatus(StatusTitulo status) {
 		this.status = status;
 	}
-
+	public boolean isPendente() {
+		return StatusTitulo.PENDENTE.equals(this.status);
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
